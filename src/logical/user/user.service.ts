f8127b5d92d093c32e38c597e78fe31a6f1b4d43
MININT-OG9HDQB\Administrator <963889833@qq.com>
2020-11-04 17:04:26 +0800

    Revert "jwt"
    This reverts commit 4ed13a5fc79a3763d70e4ac285d4a2ff0aa114f3.

--- src/logical/user/user.service.ts
+++ src/logical/user/user.service.ts
@@ -1,94 +1,30 @@
 // src/logical/user/user.service.ts
 import { Injectable } from '@nestjs/common';
-import User from '../../database/user'
-import { makeSalt, encryptPassword } from '../../utils/cryptogram'; // 引入加密函数
+import * as Sequelize from 'sequelize'; // 引入 Sequelize 库
+import sequelize from '../../database/sequelize'; // 引入 Sequelize 实例
 
 @Injectable()
 export class UserService {
-    //注册
-    async register(requestBody: any): Promise<any | undefined> {
-        const { accountName, realName, password, repassword, mobile } = requestBody;
-        if (password !== repassword) {
-            return {
-                code: 400,
-                msg: '两次密码输入不一致',
-            };
-        }
-        const user = await this.findOne(accountName);
-        if (user) {
-            return {
-                code: 400,
-                msg: '用户已存在',
-            };
-        }
-        const salt = makeSalt(); // 制作密码盐
-        const hashPwd = encryptPassword(password, salt);  // 加密密码
-        let res = await User.create({
-            account_name: accountName,
-            real_name: realName,
-            passwd: password,
-            passwd_salt: hashPwd,
-            salt: salt,
-            mobile: mobile,
-            create_by: 1,
-            create_time: '2020-11-04 11:03'
-        })
-        return res ? `新增${accountName}成功` : `新增失败`
-    }
     //查找
     async findOne(username: string): Promise<any | undefined> {
-        let res = await User.findOne({
-            where: {
-                real_name: username
-            }
-        })
-        return res ? res : undefined
-    }
-    //创建新用户
-    async createOne(account_name: string, passwd: string, mobile: number): Promise<any | undefined> {
-        let finStatus = await this.findOne(account_name)
-        if (finStatus) {
-            return `${finStatus},用户已经存在了`
+        const sql = `
+      SELECT
+        user_id id, real_name realName, role
+      FROM
+        admin_user
+      WHERE
+        account_name = '${username}'
+    `; // 一段平淡无奇的 SQL 查询语句
+        try {
+            const res = await sequelize.query(sql, {
+                type: Sequelize.QueryTypes.SELECT, // 查询方式
+                raw: true, // 是否使用数组组装的方式展示结果
+                logging: true, // 是否将 SQL 语句打印到控制台，默认为 true
+            });
+            const user = res[0]; // 查出来的结果是一个数组，我们只取第一个。
+            return user
+        } catch (error) {
+            return 0
         }
-        let salt = makeSalt()
-        const hashPwd = encryptPassword(passwd, salt)
-        let res = await User.create({
-            account_name: account_name,
-            real_name: account_name,
-            passwd: passwd,
-            passwd_salt: hashPwd,
-            salt: salt,
-            mobile: mobile,
-            create_by: 1,
-            create_time: '2020-11-04 11:03'
-        })
-        return res ? `新增${account_name}成功` : `新增失败`
-    }
-    //删除用户
-    async delOne(name: string): Promise<any | undefined> {
-        let finStatus = await this.findOne(name)
-        if (!finStatus) {
-            return `用户不存在的`
-        }
-        let res = await User.destroy({
-            where: {
-                account_name: name
-            }
-        })
-        return res ? `删除${name}成功` : `删除失败或者用户不存在`
-    }
-
-    //更新用户信息
-    async updateOne(name: string, passwd: string): Promise<any | undefined> {
-        let finStatus = await this.findOne(name)
-        if (!finStatus) {
-            return `用户不存在的`
-        }
-        let res = await User.update({ passwd: passwd }, {
-            where: {
-                account_name: name
-            }
-        })
-        return res ? `${name}信息密码成功` : `密码修改不成功`
     }
 }
