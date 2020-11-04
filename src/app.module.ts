4ed13a5fc79a3763d70e4ac285d4a2ff0aa114f3
MININT-OG9HDQB\Administrator <963889833@qq.com>
2020-11-04 17:03:13 +0800

    jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './logical/user/user.module';
+// import { AuthService } from './logical/auth/auth.service';
+import { AuthModule } from './logical/auth/auth.module';
+import { UserController } from './logical/user/user.controller';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
+  imports: [UserModule, AuthModule],
+  controllers: [AppController,UserController],
   providers: [AppService],
 })
 export class AppModule {}
